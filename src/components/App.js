d16c6fa7d3a529e3dbd3051dd16f787fae9fc62f
subhash-star <subhamjoshibtech@gmail.com>
2021-04-13 10:39:30 +0530

    another commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,65 +1,100 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
-const App =()=> {
-  
-  const [time,setTime]=useState(0);
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  const [movement, setMovement] = useState(false);
-  const [timer,setTimer]=useState();
-  // componentDidMount() { 
-  // }
-  // componentWillUnmount() { 
-  // }
-  const changePosition=(event)=>{
-    switch(event.keyCode){
-      case 39:
-          setX(x=>x+5)
-          break;
-      case 40:
-          setY(y=> y+5)
-          break;
-      case 37:
-          setX(x=> x-5)
-          break;
-      case 38:
-          setY(y=> y-5)
-          break;
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false,
+      time: 0,
+      x: 0,
+      y: 0,
+      ballPosition: {
+        left: "0px",
+        top: "0px",
+      },
+    };
+    this.renderChoice = this.renderChoice.bind(this);
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+    this.handleKey = this.handleKey.bind(this);
+  }
+  buttonClickHandler() {
+    this.setState({ renderBall: true });
+    document.addEventListener("keydown", this.handleKey);
+    this.interval = setInterval(() => {
+      this.setState({ time: this.state.time + 1 });
+    }, 1000);
+  }
+
+  renderChoice() {
+    return (
+      <div>
+        <div className="ball" style={this.state.ballPosition}></div>
+        <div className="hole"></div>
+        <div className="heading-timer">{this.state.time}</div>
+
+        <button
+          className="start ballProvider"
+          onClick={this.buttonClickHandler}
+        >
+          Click For Start
+        </button>
+      </div>
+    );
+  }
+
+  componentDidMount() {}
+
+  componentDidUpdate() {
+    if (this.state.x === 250 && this.state.y === 250) {
+      clearInterval(this.interval);
+      document.removeEventListener("keydown", this.handleKey);
     }
   }
-  useEffect(()=>{
-    if(x==250 && y==250){
-      setMovement(movement=> false); 
-      // REMOVE SETINTERVAL
-      clearInterval(timer);
+
+  handleKey(event) {
+    if (event.keyCode === 39) {
+      this.setState({ x: this.state.x + 5 }, () =>
+        this.setState({
+          ballPosition: {
+            left: this.state.x + "px",
+            top: this.state.y + "px",
+          },
+        })
+      );
+    } else if (event.keyCode === 40) {
+      this.setState({ y: this.state.y + 5 }, () =>
+        this.setState({
+          ballPosition: {
+            left: this.state.x + "px",
+            top: this.state.y + "px",
+          },
+        })
+      );
+    } else if (event.keyCode === 38) {
+      this.setState({ y: this.state.y - 5 }, () =>
+        this.setState({
+          ballPosition: {
+            left: this.state.x + "px",
+            top: this.state.y + "px",
+          },
+        })
+      );
+    } else if (event.keyCode === 37) {
+      this.setState({ x: this.state.x - 5 }, () =>
+        this.setState({
+          ballPosition: {
+            left: this.state.x + "px",
+            top: this.state.y + "px",
+          },
+        })
+      );
     }
-    if(movement){
-      document.addEventListener("keydown",changePosition);
-      return()=>(
-        document.removeEventListener("keydown",changePosition)
-      )
-    }
-  },[movement,x,y])
-  
-  const startMovement=()=>{
-    setMovement(movement=> true);
-    //console.log(time) 
-    setTimer(timer=> setInterval(() => {
-      setTime(time=>time+1)
-    },1000))
   }
-  useEffect(()=>{
-    document.querySelector('.heading-timer').innerText=`${time}`;    
-  },[time])
-  return (
-    <>
-      <div className="ball" style={{left:x+"px" , top:y+"px"}}></div>
-      <div className="hole"></div>
-      <div className="heading-timer">0</div>
-      <button className="start" onClick={startMovement}>start</button>
-    </>
-  );
-  
+
+  render() {
+    return <div className="playground">{this.renderChoice()}</div>;
+  }
 }
 
-export default App;
+export default Timer;
